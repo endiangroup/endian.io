0801eeab8ed719c26b12af9bab2cdb118daaa83f
Paul M Fox <iam@yourheropaul.com>
2019-01-15 12:03:38 +0000

    Navigation (1)

--- themes/sherpa/scripts/app.ts
+++ themes/sherpa/scripts/app.ts
@@ -1,1 +1,15 @@
-// $(document).foundation()
+$(document).foundation()
+
+var buffer = $('.buffer');
+var header = $('header');
+buffer.height(header.height());
+
+$('.top-section').on('sticky.zf.stuckto:top', function(){
+$(this).addClass('shrink');
+$('.buffer').show();
+$('header').hide();
+}).on('sticky.zf.unstuckfrom:top', function(){
+  $(this).removeClass('shrink');
+$('header').slideDown();
+$('.buffer').slideUp();
+})
